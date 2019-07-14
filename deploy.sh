d637c5eda1cd86255cc5348403eea05b441b4dad
Jeremy LeCroy <jeremy212@gmail.com>
2019-07-13 23:42:49 -0400

    3rd time!

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,10 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# update submodulea
+git submodule update --remote --merge
+
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
